cddbea862a3edaf7cc4a6b18590114886fd8fd12
Muhammad Alfian Rasyidin <13516104@std.stei.itb.ac.id>
2018-04-11 07:37:31 +0700

    add more drivers

---
+++ src/driver/GuppyDriver.cpp
@@ -0,0 +1,12 @@
+// Guppy Driver
+#include "../Guppy.hpp"
+
+int main()
+{
+    // Guppy guppy(10.0, 10.0);
+
+    // /// Test Script
+    // cout << "Guppy x position: " << guppy.getX() << endl;
+    // cout << "Guppy y position: " << guppy.getY() << endl;
+    // cout << "Guppy radius: " << guppy.getRadius() << endl;
+}
---
+++ src/driver/PiranhaDriver.cpp
@@ -0,0 +1,11 @@
+// Piranha Driver
+#include "../Piranha.hpp"
+
+int main(){
+    // Piranha piranha = new Piranha();
+
+    /// Test Script
+    // cout << "Piranha x position: " << piranha.getX() << endl;
+    // cout << "Piranha y position: " << piranha.getY() << endl;
+    // cout << "Piranha radius: " << piranha.getRadius() << endl;
+}
